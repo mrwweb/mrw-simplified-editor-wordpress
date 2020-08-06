065e37508f4b8538eb9ee824f6fa1f4f1c23d0c7
Mark Root-Wiley <info@mrwweb.com>
2020-08-05 17:15:00 -0700

    prepare readme for 2.2.0 release, moving old changelog entries to changelog.txt, edit readme aggressively to get it under 10KB

--- mrwweb-simple-tinymce.php
+++ mrwweb-simple-tinymce.php
@@ -3,9 +3,9 @@
 * Plugin Name: MRW Simplified Editor (formerly MRW Web Design Simple TinyMCE)
 * Plugin URI: https://MRWweb.com/wordpress-plugins/mrw-web-design-simple-tinymce/
 * Description: Streamlines the Block Editor and Classic Editor with only the critical features for consistent semantic formatting.
-* Version: 2.1.0
+* Version: 2.2.0
 * Author: Mark Root-Wiley
-* Author URI: http://MRWweb.com
+* Author URI: https://MRWweb.com
 * Text Domain: mrw-web-design-simple-tinymce
 * License: GPLv2 or later
 */
